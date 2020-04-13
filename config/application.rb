cac44d55c0538cd13441e35948299d4330d4f552
Lucian Rossoni Ribas <ribas.lucian@gmail.com>
2020-04-13 15:10:55 -0300

    up

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,18 @@ module MyChat
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    
+  
+    config.action_cable.url = 'http://localhost:3000/cable'
+    config.web_socket_server_url = 'ws://localhost:3000/cable'
+    #  config.action_cable.allowed_request_origins = [
+    #     # Local address of our RoR server
+    #    'http://localhost:3000',
+    #     # Local address we use for our React standalone client
+    #    'http://localhost:8000',
+    #  ]
+  
+    config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/, /ws:\/\/*/, /wss:\/\/*/]
+    config.action_cable.disable_request_forgery_protection = true
   end
 end
