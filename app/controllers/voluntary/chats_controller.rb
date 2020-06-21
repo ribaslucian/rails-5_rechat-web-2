d6ef3ca48f6ffcecdfb27b4d645101b64093fd45
Lucian Rossoni Ribas <ribas.lucian@gmail.com>
2020-06-21 15:01:20 -0300

    up

--- app/controllers/voluntary/chats_controller.rb
+++ app/controllers/voluntary/chats_controller.rb
@@ -5,7 +5,10 @@ class Voluntary::ChatsController < Voluntary::VoluntaryController
     
 #    return d params[:contact_id]
     
-    Contact.sql("UPDATE messages SET chat_open = TRUE WHERE contact_id = #{params[:contact_id]}")
+#    Contact.sql("UPDATE messages SET chat_open = TRUE WHERE contact_id = #{params[:contact_id]}")
+    
+    
+    Contact.update_all(chat_open: true, contact_id: params[:contact_id])
   end
   
 end
