d52f5d0fb7e6e80676d639cc57659158f39110c1
Lucian Ribas <ribas.lucian@gmail.com>
2020-07-01 15:03:36 -0300

    up

--- app/controllers/researcher/reports_controller.rb
+++ app/controllers/researcher/reports_controller.rb
@@ -3,12 +3,12 @@ class Researcher::ReportsController < Researcher::ResearcherController
   layout 'print'
   
   def users
-    
+    @users = User.all.order :id
   end
   
   
   def bots
-    
+    @interactions = Interaction.order(created_at: :asc).all
   end
   
   
